08c3bd2440e9856d7b31538c5dc451baa769fa3a
Alan Gewerc <agew0001@student.monash.edu>
2021-01-08 19:51:55 +0800

    testing

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ import pandas as pd
 data = pd.read_csv('race_dataset.csv', names = ['raceType', 'raceNumber', 'raceName', 'venue', 'raceStartTime'])
 
 app = flask.Flask(__name__)
-@app.route('/', methods= ['GET'])
 
+@app.route('/', methods= ['GET'])
 def home():
     dayTime = request.args['dayTime']
     try:
          new_data = data[data['raceStartTime'] > dayTime]
          return head(new_data)
     except KeyError
-        return f'Invalid Input'
+        return 'Invalid Input'
