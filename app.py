fd5856e8415b457edf7e1dbb06efcbed2aeffe08
Alan Gewerc <agew0001@student.monash.edu>
2021-01-08 18:38:34 +0800

    Flask API - create requirements - importing libraries

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
 import flask
+import pandas as pd
+from datetime import datetime
+
+data = pd.read_csv('race_dataset.csv', names = ['raceType', 'raceNumber', 'raceName', 'venue', 'raceStartTime']).set_index('raceStartTime')
+
+series = pd.series(index = range(data.raceStartTime.min(), data.raceStartTime.max()))
+for m in data.index:
+    series.loc[data.loc[m].s:data.loc[m].e] = m
 
 app = flask.Flask(__name__)
 
 @app.route('/', methods= ['GET'])
 
 def home():
-    arg = request.args['arg1']
-    return 'Hello World'
+    dayTime = request.args['dayTime']
+    try:
+        return series.loc[dayTime]
+    except KeyError
+        return f'Invalid Input'
+
+
+
+@app.route('')
