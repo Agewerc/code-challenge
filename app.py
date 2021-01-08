68a29eacda3e09fca53917a212e407f1073b5982
Alan Gewerc <agew0001@student.monash.edu>
2021-01-08 20:29:50 +0800

    check identiation second fix

--- app.py
+++ app.py
@@ -2,15 +2,15 @@ import flask
 from flask import request
 import pandas as pd
 
-data = pd.read_csv('race_dataset.csv', names = ['raceType', 'raceNumber', 'raceName', 'venue', 'raceStartTime'])
+data = pd.read_csv('race_dataset.csv', names=['raceType', 'raceNumber', 'raceName', 'venue', 'raceStartTime'])
 
 app = flask.Flask(__name__)
 
-@app.route('/', methods=['GET'])
 
+@app.route('/', methods=['GET'])
 def home():
     dayTime = float(request.args['dayTime'])
     try:
-        return data[data['raceStartTime'] > float(dayTime)].head()
+        return data[data['raceStartTime'] > dayTime].head()
     except KeyError:
         return 'Invalid Input'
